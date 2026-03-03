66afda821f18f5cc76c25c1c369532567e020205
Sanaa Nicola <sanaa.nicolas@gmail.com>
2026-03-03 15:58:44 -0500

    chore: setup Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './layout/*.liquid',
+    './templates/**/*.{liquid,json}',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './assets/*.js',
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
